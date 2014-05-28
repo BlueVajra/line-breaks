f7116e30999c7da416fb9325699ef7bfa589bd0e
BlueVajra <cory.leistikow@gmail.com>
2014-05-28 09:38:36 -0600

    user can break lines by a default of 80 chars

--- lib/line_breaker.rb
+++ lib/line_breaker.rb
@@ -1,27 +1,23 @@
 class LineBreaker
-  def initialize(text, char_number)
+  def initialize(text, char_number=80)
     @text = text
     @char_number = char_number
   end
 
   def broken_lines
-    #read through lines
-    #for each line check for the 20th char
-    #if the 20th char is whitespace, break line,
-    #if not, move backwards til it is
     new_string = ""
 
     @text.each_line do |line|
       new_line = line
 
-      num=20
+      num=@char_number
 
       until num > line.length
         until line[num] =~ /\s/
           num = num - 1
         end
         new_line[num] = "\n"
-        num = num + 20
+        num = num + @char_number
       end
 
       new_string = new_string + new_line
--- spec/line_breaker_spec.rb
+++ spec/line_breaker_spec.rb
@@ -1,6 +1,25 @@
 require 'line_breaker'
 
 describe LineBreaker do
+  it "breaks up lines at default-80 length" do
+
+    text = <<TEXT
+Jean shorts you probably haven't heard of them farm-to-table. Jean shorts you probably haven't heard of them farm-to-table. Jean shorts you probably haven't heard of them farm-to-table.
+
+Another line.
+TEXT
+    expected = <<TEXT
+Jean shorts you probably haven't heard of them farm-to-table. Jean shorts you
+probably haven't heard of them farm-to-table. Jean shorts you probably haven't
+heard of them farm-to-table.
+
+Another line.
+TEXT
+
+    broken_text = LineBreaker.new(text)
+    actual = broken_text.broken_lines
+    expect(actual).to eq expected
+  end
 
   it "breaks up lines at specificied length" do
 
@@ -22,6 +41,4 @@ TEXT
     actual = broken_text.broken_lines
     expect(actual).to eq expected
   end
-
 end
-
